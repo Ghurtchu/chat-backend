d240e861d29effce5cc6be078826dbe078f7e4f3
Anzori (Nika) Gurchumelia <aghurtchumelia@Anzoris-MacBook-Pro.local>
2023-09-18 22:22:15 +0200

    load dynamic data based on scrolling mechanism

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,12 +11,8 @@ import org.http4s.server.websocket.WebSocketBuilder2
 import org.http4s.websocket.WebSocketFrame
 import doobie._
 import doobie.implicits._
-import cats._
-import cats.effect._
-import cats.implicits._
 
 import java.time.Instant
-import scala.concurrent.duration.DurationInt
 
 // psql -h localhost -p 5432 -U aghurtchumelia -d chat
 // db-name: chat
@@ -63,52 +59,19 @@ object Main extends IOApp {
         .useForever
     } yield ExitCode.Success
 
-  val pipe: Pipe[IO, WebSocketFrame, WebSocketFrame] = _.collect { case WebSocketFrame.Text(msg, _) =>
-    WebSocketFrame.Text(msg)
-  }.evalMap {
-    case WebSocketFrame.Text(msg, _) if msg.trim == "time" =>
-      Clock[IO].realTimeInstant.map(t => WebSocketFrame.Text(t.toString))
-    case other                                             => IO.pure(other)
-  }.merge {
-    Stream
-      .awakeEvery[IO](5.seconds)
-      .map(duration => WebSocketFrame.Text(s"Connected for $duration"))
-  }
-
   private def httpApp(
     topic: Topic[IO, Msg],
     wsb: WebSocketBuilder2[IO],
   ): HttpApp[IO] =
-    (echo(wsb) <+> chats(topic, wsb) <+> chat(topic, wsb)).orNotFound
-
-  private def echo(wsb: WebSocketBuilder2[IO]): HttpRoutes[IO] = {
-    val dsl = new Http4sDsl[IO] {}
-    import dsl._
-
-    HttpRoutes.of[IO] {
-      // for testing
-      // websocat "ws://localhost:9000/echo"
-      case GET -> Root / "echo" =>
-        for {
-          // Unbounded queue to store websocket messages from the client, which are pending to be processed.
-          // For production use bounded queue seems a better choice. Unbounded queue may result in OOM (out of memory error)
-          // if the client is sending messages quicker than the server can process them.
-          queue    <- Queue.unbounded[IO, WebSocketFrame]
-          response <- wsb.build(
-            send = Stream.repeatEval(queue.take).through(pipe),
-            receive = _.evalMap(queue.offer),
-          )
-        } yield response
-    }
-  }
+    (chats(topic, wsb) <+> chat(topic, wsb)).orNotFound
 
   trait LoadConvos {
-    def load(userId: String, lastN: Int): IO[List[Conversation]]
+    def load(userId: Int, lastN: Int): IO[List[Conversation]]
   }
 
   val loadConvos = new LoadConvos {
 
-    override def load(userId: String, lastN: Int): IO[List[Conversation]] = {
+    override def load(userId: Int, lastN: Int): IO[List[Conversation]] = {
 
       val query =
         sql"""
@@ -136,7 +99,7 @@ object Main extends IOApp {
             message m
             ON max_message.conversation_id = m.conversation_id
         WHERE
-            u.user_id = 1 -- Replace with the actual user ID
+            u.user_id = $userId
         ORDER BY
             c.conversation_id,
             m.written_at DESC
@@ -171,7 +134,7 @@ object Main extends IOApp {
         .subscribe(maxQueued = 10)
         .evalMap { case Msg.LoadConversations(n) =>
           loadConvos
-            .load(userId, n)
+            .load(userId.trim.toInt, n)
             .map(convos => WebSocketFrame.Text(convos.mkString("[", ",", "]")))
 
 //          case Msg.LoadConversations(from, to) =>
@@ -195,7 +158,7 @@ object Main extends IOApp {
         // Sink, where the incoming WebSocket messages from the client are pushed to
         receive = topic.publish.compose[Stream[IO, WebSocketFrame]](_.collect {
           case WebSocketFrame.Text(msg, _) =>
-            val amount  = msg.trim.toInt
+            val amount = msg.trim.toInt
 
             Msg.LoadConversations(amount)
         }),
