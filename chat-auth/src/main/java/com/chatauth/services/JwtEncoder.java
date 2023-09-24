2e0c3499a4a678b175655687d28a5f3d05851fe4
Mate <arevadzemate@gmail.com>
2023-09-24 22:02:19 +0400

    Changes by aghurtchumelia

--- chat-auth/src/main/java/com/chatauth/services/JwtEncoder.java
+++ chat-auth/src/main/java/com/chatauth/services/JwtEncoder.java
@@ -1,8 +1,33 @@
 package com.chatauth.services;
 
+import com.chatauth.domain.User;
+import io.jsonwebtoken.Claims;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
 /**
  * generates JWT token based on username
  */
 public interface JwtEncoder {
-  String encode(String username);
+  String extractUsername(String token);
+
+  Claims extractAllClaims(String token);
+
+  <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+
+  Key getSignInKey();
+
+  String generateToken(Map<String, Object> extraClaims,
+                       User user);
+
+  String generateToken(User user);
+
+  boolean isTokenValid(String token, User user);
+
+  boolean isTokenExpired(String token);
+
+  Date extractExpiration(String token);
 }
